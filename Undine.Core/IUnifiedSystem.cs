57e5bf7ba8c02902d73f22e1577b4dac670b496c
Tomasz Cekało <tomasz.cekalo@gmail.com>
2024-01-02 21:46:50 +0100

    removing systems from folders

--- Undine.Core/IUnifiedSystem.cs
+++ Undine.Core/IUnifiedSystem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Undine.Core.Unmanaged
+namespace Undine.Core
 {
     public interface IUnifiedSystem
     {
@@ -12,31 +12,21 @@ namespace Undine.Core.Unmanaged
     }
 
     public interface IUnifiedSystem<T> : IUnifiedSystem
-        where T : unmanaged
     {
         void ProcessSingleEntity(int entityId, ref T t);
     }
 
     public interface IUnifiedSystem<A, B> : IUnifiedSystem
-        where A : unmanaged
-        where B : unmanaged
     {
         void ProcessSingleEntity(int entityId, ref A a, ref B b);
     }
 
     public interface IUnifiedSystem<A, B, C> : IUnifiedSystem
-        where A : unmanaged
-        where B : unmanaged
-        where C : unmanaged
     {
         void ProcessSingleEntity(int entityId, ref A a, ref B b, ref C c);
     }
 
     public interface IUnifiedSystem<A, B, C, D> : IUnifiedSystem
-        where A : unmanaged
-        where B : unmanaged
-        where C : unmanaged
-        where D : unmanaged
     {
         void ProcessSingleEntity(int entityId, ref A a, ref B b, ref C c, ref D d);
     }
--- Undine.Core/UnifiedSystem.cs
+++ Undine.Core/UnifiedSystem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Undine.Core.Class
+namespace Undine.Core
 {
     public abstract class UnifiedSystem : IUnifiedSystem
     {
@@ -13,35 +13,25 @@ namespace Undine.Core.Class
         public virtual void PostProcess()
         {
         }
-    }
 
+    }
     public abstract class UnifiedSystem<T> : UnifiedSystem, IUnifiedSystem<T>
-        where T : class
 
     {
         public abstract void ProcessSingleEntity(int entityId, ref T t);
     }
 
     public abstract class UnifiedSystem<A, B> : UnifiedSystem, IUnifiedSystem<A, B>
-        where A : class
-        where B : class
     {
         public abstract void ProcessSingleEntity(int entityId, ref A a, ref B b);
     }
 
     public abstract class UnifiedSystem<A, B, C> : UnifiedSystem, IUnifiedSystem<A, B, C>
-        where A : class
-        where B : class
-        where C : class
     {
         public abstract void ProcessSingleEntity(int entityId, ref A a, ref B b, ref C c);
     }
 
     public abstract class UnifiedSystem<A, B, C, D> : UnifiedSystem, IUnifiedSystem<A, B, C, D>
-        where A : class
-        where B : class
-        where C : class
-        where D : class
     {
         public abstract void ProcessSingleEntity(int entityId, ref A a, ref B b, ref C c, ref D d);
     }
